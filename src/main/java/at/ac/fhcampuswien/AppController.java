5bc8a321b1ce7e23913c32d92f8c767133f597f7
DESKTOP-78E4C6R\schwa <sch18344@spengergasse.at>
2022-03-02 13:13:16 +0100

    - Updated gitignore - renamed AppTest to AppControllerTest - added methods for AppController (without function) - added 3 unit tests for AppController.getArticleCount

--- src/main/java/at/ac/fhcampuswien/AppController.java
+++ src/main/java/at/ac/fhcampuswien/AppController.java
@@ -1,4 +1,53 @@
 package at.ac.fhcampuswien;
 
-public class AppController {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppController
+{
+    private List<Article> articles;
+
+    public AppController()
+    {
+
+    }
+
+    //region SETTER ----------------------------------------------------------------------------------------------------
+
+    public void setArticles(List<Article> articles)
+    {
+
+    }
+
+    //endregion
+
+    //region GETTER ----------------------------------------------------------------------------------------------------
+    public int getArticleCount()
+    {
+        return 0;
+    }
+
+    public List<Article> getTopHeadLinesAustria()
+    {
+        return new ArrayList<>();
+    }
+
+    public List<Article> getAllNewsBitcoin()
+    {
+        return new ArrayList<>();
+    }
+
+    //endregion
+
+    //region METHODS ---------------------------------------------------------------------------------------------------
+    protected List<Article> filterList(String query, List<Article> articles)
+    {
+        return new ArrayList<>();
+    }
+
+    public static List<Article> generateMockList()
+    {
+        return new ArrayList<>();
+    }
+    //endregion
 }
---
+++ src/test/java/at/ac/fhcampuswien/AppControllerTest.java
@@ -0,0 +1,70 @@
+package at.ac.fhcampuswien;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AppControllerTest
+{
+
+    @Test
+    public void firstTest(){
+        App myApp = new App();
+        String actual = myApp.welcomeMessage();
+        assertEquals("Hello World", actual);
+    }
+
+    /**
+     * Test if article count is 3 when there are 3 articles
+     */
+    @Test
+    public void getArticleCountTest1()
+    {
+        AppController appController = new AppController();
+        List<Article> articles = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            articles.add(new Article());
+        }
+
+        appController.setArticles(articles);
+
+        assertEquals(3, appController.getArticleCount());
+    }
+
+    /**
+     * Test if article count is 6 when there are 6 articles
+     */
+    @Test
+    public void getArticleCountTest2()
+    {
+        AppController appController = new AppController();
+        List<Article> articles = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            articles.add(new Article());
+        }
+
+        appController.setArticles(articles);
+
+        assertEquals(6, appController.getArticleCount());
+    }
+
+    /**
+     * Test if article count is 0 when null
+     */
+    @Test
+    public void getArticleCountTest3()
+    {
+        AppController appController = new AppController();
+        appController.setArticles(null);
+
+        assertEquals(0, appController.getArticleCount());
+    }
+
+
+}
