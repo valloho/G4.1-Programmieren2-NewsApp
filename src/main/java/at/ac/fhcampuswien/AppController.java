5d19453b546847595700ff4b1d5e937160caaf0f
schwalmentin <schwalm@gmx.at>
2022-03-28 20:25:02 +0200

    - added function to getArticleCount - added comments to all methods

--- src/main/java/at/ac/fhcampuswien/AppController.java
+++ src/main/java/at/ac/fhcampuswien/AppController.java
@@ -3,6 +3,9 @@ package at.ac.fhcampuswien;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Manages a list of articles
+ */
 public class AppController
 {
     private List<Article> articles;
@@ -14,6 +17,10 @@ public class AppController
 
     //region SETTER ----------------------------------------------------------------------------------------------------
 
+    /**
+     * Sets the current article list to a given article list.
+     * @param articles List of articles to set
+     */
     public void setArticles(List<Article> articles)
     {
 
@@ -22,16 +29,33 @@ public class AppController
     //endregion
 
     //region GETTER ----------------------------------------------------------------------------------------------------
+
+    /**
+     * @return The amount of articles
+     */
     public int getArticleCount()
     {
-        return 0;
+        if (articles == null)
+        {
+            return 0;
+        }
+
+        return this.articles.size();
     }
 
+    /**
+     * Not implemented yet!
+     * @return Empty array list of articles
+     */
     public List<Article> getTopHeadLinesAustria()
     {
         return new ArrayList<>();
     }
 
+    /**
+     * Searches for all articles with the query "bitcoin".
+     * @return A list of articles about bitcoins
+     */
     public List<Article> getAllNewsBitcoin()
     {
         return new ArrayList<>();
@@ -40,11 +64,22 @@ public class AppController
     //endregion
 
     //region METHODS ---------------------------------------------------------------------------------------------------
+
+    /**
+     * Searches a list of articles with a given query in the title.
+     * @param query What articles are searched
+     * @param articles Where articles are searched
+     * @return List of all articles with the given query
+     */
     protected List<Article> filterList(String query, List<Article> articles)
     {
         return new ArrayList<>();
     }
 
+    /**
+     * Generates a list with dummy articles.
+     * @return List of dummy articles
+     */
     public static List<Article> generateMockList()
     {
         return new ArrayList<>();
