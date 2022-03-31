64c1eb58a5f5bc9c50b5b3dd2397443c20f93eab
brdick <72357873+brdick@users.noreply.github.com>
2022-03-31 18:49:43 +0200

    Update AppControllerTest.java
    add filterList tests

--- src/test/java/at/ac/fhcampuswien/AppControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/AppControllerTest.java
@@ -99,4 +99,40 @@ public class AppControllerTest
 
     }
 
+    @Test
+    public void filterListTest1(){
+
+        AppController appController = new AppController();
+        List<Article> articleList = new ArrayList<Article>();
+        articleList.add(new Article("bobby", "bobby's great adventure"));
+        articleList.add(new Article("boberts", "boberts' amazing adventure"));
+        appController.setArticles(articleList);
+
+        assertEquals(articleList.get(0), appController.filterList("bobby", articleList).get(0));
+    }
+
+    @Test
+    public void filterListTest2(){
+
+        AppController appController = new AppController();
+        List<Article> articleList = new ArrayList<Article>();
+        articleList.add(new Article("bobby", "bobby's great adventure"));
+        articleList.add(new Article("bobby", "bobby's amazing adventure"));
+        articleList.add(new Article("bobby", "bobby's amazing adventure 2"));
+        appController.setArticles(articleList);
+
+        assertEquals(articleList, appController.filterList("bobby", articleList));
+    }
+
+    @Test
+    public void filterListTest3(){
+
+        AppController appController = new AppController();
+        List<Article> articleList = new ArrayList<Article>();
+        articleList.add(new Article("bobby", "bobby's great adventure"));
+        articleList.add(new Article("boberts", "boberts' amazing adventure"));
+        appController.setArticles(articleList);
+
+        assertEquals(null, appController.filterList("boberta", articleList));
+    }
 }
