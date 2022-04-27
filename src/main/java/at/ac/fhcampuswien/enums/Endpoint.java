1bcd26321428841d87eaa45f9a76d410a2adc4df
Nico-Schulterer <nicolassch2010@gmail.com>
2022-04-27 17:21:46 +0200

    endpoint enum added

--- src/main/java/at/ac/fhcampuswien/enums/Endpoint.java
+++ src/main/java/at/ac/fhcampuswien/enums/Endpoint.java
@@ -1,4 +1,13 @@
 package at.ac.fhcampuswien.enums;
 
 public enum Endpoint {
+    EVERYTHING("everything"),
+    TOP_HEADLINES("top-headlines"),
+    SOURCES("sources");
+
+    public final String endpoint;
+
+    Endpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
 }
