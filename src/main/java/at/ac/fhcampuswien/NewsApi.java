7b192f0e1248f1a23d850c6d0658ca60ec933d46
schwalmentin <schwalm@gmx.at>
2022-04-29 14:02:15 +0200

    GetEverthing + GetTopHeadlines + GetSources

--- src/main/java/at/ac/fhcampuswien/NewsApi.java
+++ src/main/java/at/ac/fhcampuswien/NewsApi.java
@@ -1,19 +1,46 @@
 package at.ac.fhcampuswien;
 
-import at.ac.fhcampuswien.enums.Language;
+import at.ac.fhcampuswien.enums.*;
 import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.request.EverythingRequest;
+import com.kwabenaberko.newsapilib.models.request.SourcesRequest;
 import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
 import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
+import com.kwabenaberko.newsapilib.models.response.SourcesResponse;
 
 public class NewsApi {
 
-    NewsApiClient newsApiClient = new NewsApiClient("f4d4ec56134741e8a937aa730e1ca155");
+    private static final String everything = "https://newsapi.org/v2/everything?";
+    private static final String topHeadlines = "https://newsapi.org/v2/top-headlines?";
+    private static final String sources = "https://newsapi.org/v2/top-headlines/sources?";
+    private static final String apiKey = "f4d4ec56134741e8a937aa730e1ca155";
 
-    public void getTopHeadlines(String q, Language language) {
-        newsApiClient.getTopHeadlines(
-                new TopHeadlinesRequest.Builder()
+    NewsApiClient newsApiClient = new NewsApiClient(apiKey);
+
+    public String getURL(Endpoint endPoint, String q, Language language, SortBy sortBy, Country country, Category category)
+    {
+        String url = "";
+
+        switch (endPoint)
+        {
+            case SOURCES -> url = sources + "category=" + category + "&language=" + language + "&country=" + country;
+
+            case EVERYTHING -> url = everything + "q=" + q + "&language=" + language + "&sortBy=" + sortBy + "&apiKey=" + apiKey;
+
+            case TOP_HEADLINES -> url = topHeadlines + "country="+ country + "&category=" + category + "&q=" + q + "&apiKey=" + apiKey;
+        }
+
+        return url;
+    }
+
+    public NewsResponse everyThing(String q, Language language, SortBy sortBy)
+    {
+        // /v2/everything
+        newsApiClient.getEverything(
+                new EverythingRequest.Builder()
                         .q(q)
-                        .language(String.valueOf(language))
+                        .language(language.label)
+                        .sortBy(sortBy.sortby)
                         .build(),
                 new NewsApiClient.ArticlesResponseCallback() {
                     @Override
@@ -27,5 +54,58 @@ public class NewsApi {
                     }
                 }
         );
+
+        return null;
+    }
+
+    public NewsResponse getTopHeadlines(String q, Language language, Country country, Category category)
+    {
+        // /v2/top-headlines
+        newsApiClient.getTopHeadlines(
+                new TopHeadlinesRequest.Builder()
+                        .q(q)
+                        .language(language.label)
+                        .category(category.category)
+                        .country(country.countrycode)
+                        .build(),
+
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+                        System.out.println(response.getArticles().get(0).getTitle());
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        System.out.println(throwable.getMessage());
+                    }
+                }
+        );
+        return null;
+    }
+
+    public NewsResponse getSources(Language language, Country country, Category category)
+    {
+        // /v2/top-headlines/sources
+        newsApiClient.getSources(
+                new SourcesRequest.Builder()
+                        .language(language.label)
+                        .country(country.countrycode)
+                        .category(category.category)
+                        .build(),
+                new NewsApiClient.SourcesCallback() {
+                    @Override
+                    public void onSuccess(SourcesResponse response) {
+                        System.out.println(response.getSources().get(0).getName());
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        System.out.println(throwable.getMessage());
+                    }
+                }
+        );
+
+        return null;
     }
 }
