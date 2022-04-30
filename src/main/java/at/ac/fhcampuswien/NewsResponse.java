02d8621c37e98ff1e55c3915fcd5d68477c0a2f8
brdick <72357873+brdick@users.noreply.github.com>
2022-04-30 04:11:43 +0200

    Update NewsResponse.java

--- src/main/java/at/ac/fhcampuswien/NewsResponse.java
+++ src/main/java/at/ac/fhcampuswien/NewsResponse.java
@@ -1,6 +1,22 @@
 package at.ac.fhcampuswien;
 
+import java.util.ArrayList;
+
 public class NewsResponse
 {
+    String status = "";
+    int totalResult = 0;
+    ArrayList<Article> articles = new ArrayList<>();
 
+    public String getStatusString() {
+        return status;
+    }
+
+    public int getTotalResults() {
+        return totalResult;
+    }
+    
+    public ArrayList<Article> getArticles() {
+        return articles;
+    }
 }
